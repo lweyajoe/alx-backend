9a2015dc25fdfe8e0b73da4ace2793d159b51142
Joseph Lweya <84285814+lweyajoe@users.noreply.github.com>
2024-01-15 11:19:40 +0300

    Update 1-redis_op.js

---
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -0,0 +1,29 @@
+// Redis Client
+
+import redis from 'redis';
+
+const client = redis.createClient();
+
+client.on('error', (error) => {
+  console.log(`Redis client not connected to the server: ${error.message}`);
+});
+
+function setNewSchool(schoolName, value) {
+  client.set(schoolName, value, redis.print);
+}
+
+function displaySchoolValue(schoolName) {
+  client.get(schoolName, (_, res) => {
+    console.log(res);
+  });
+}
+
+client.on('connect', () => {
+    console.log('Redis client connected to the server');
+});
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
+
+module.exports = client;
